1451ec9fa53e8aa723c7665746e15842da92d24e
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-20 12:17:25 +0200

    Add countdown function

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import { createElement } from "./utils/elements";
 // import playBtnImg from "./play.png";
 // import pauseBtnImg from "./pause.png";
 
+let counter = 10;
+
+function countDown() {
+  counter--;
+}
+
 export function createTimer() {
   //   const playBtnAction = createElement("img", {
   //     src: playBtnImg,
   //   });
+
   const TimerContainer = createElement("div", {
     className: "timer",
     children: [
       createElement("button", {
-        innerText: "Play",
+        innerText: "Start",
         className: "timer__btn",
+        onclick: () => {
+          countDown(counter);
+          console.log(counter);
+        },
         // children: [playBtnAction],
       }),
       createElement("input", {
         className: "timer__input",
       }),
       createElement("div", {
+        innerText: counter,
         className: "timer__display",
       }),
     ],
